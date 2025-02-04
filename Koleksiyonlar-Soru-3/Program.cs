9ea5feab09959eae63ba62ac10096d8786a35de1
serhattsakalli <129197692+serhattsakalli@users.noreply.github.com>
2025-02-04 15:13:01 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Koleksiyonlar_Soru_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde 
+        //saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+        List<char> Sesliler =new List<char>();
+        string cumle="";
+        string seslihrflr="aeıioöuü";
+        Console.WriteLine("Cümle giriniz");
+        cumle=Console.ReadLine();
+        foreach (char item in cumle)
+        {
+            if (seslihrflr.Contains(char.ToLower(item)))
+            {
+                Sesliler.Add(item);
+            }
+        }
+        Sesliler.Sort();
+        Console.WriteLine("Cümledeki sesli harfler: ");
+            foreach (var harf in Sesliler)
+            {
+                Console.Write(harf + " ");
+            }
+
+
+
+        Console.ReadLine();
+    }
+}
